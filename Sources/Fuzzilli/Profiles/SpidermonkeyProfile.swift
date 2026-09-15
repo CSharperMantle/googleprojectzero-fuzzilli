5083d7d234492b2a8b43caeb0fd297c33b95c0ea
Rong Bao <rong.bao@csmantle.top>
2026-09-15 11:59:48 +0800

    Remove nonexistent enqueueJob builtin in SM profile

--- Sources/Fuzzilli/Profiles/SpidermonkeyProfile.swift
+++ Sources/Fuzzilli/Profiles/SpidermonkeyProfile.swift
@@ -116,10 +116,8 @@ let spidermonkeyProfile = Profile(
 
     additionalBuiltins: [
         "gc": .function([] => .undefined),
-        "enqueueJob": .function([.function()] => .undefined),
         "drainJobQueue": .function([] => .undefined),
         "bailout": .function([] => .undefined),
-
     ],
 
     additionalObjectGroups: [],
